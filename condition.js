87855f9efce426d57512172f93bb64d1b81d1b80
Nikhil Kuntal <nikhilkuntal@Nikhils-MacBook-Air.local>
2026-03-09 16:05:17 +0530

    done

---
+++ condition.js
@@ -0,0 +1,23 @@
+// let temp = 25;
+// if(temp > 30){
+//     console.log("its too hot");
+// }else if(temp>20){
+// console.log("its warm");
+// }else{
+//     console.log("it's cold");
+// }
+
+// let day=4;
+// switch(day){
+//     case 1:
+//         console.log("monday");
+//         break;
+
+//         case 2:
+//         console.log("tuesday");
+//         break;
+
+//         default:
+//             console.log("invalid day");
+// }
+
---
+++ script.js
@@ -0,0 +1,47 @@
+const { log } = require("console");
+
+console.log("hello world");
+
+
+let name = "Alice";        
+let age = 30;              
+let isStudent = true;      
+let score;                 
+let car = null;            
+let person = { name: "Bob", age: 25 }; 
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(score);
+console.log(car);
+console.log(person);
+
+
+let a=10;
+let b=20;
+console.log(a+b);
+console.log(a-b);   
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+let c=10;
+c+=5;
+console.log(c);
+c-=3;
+console.log(c);
+c*=2;
+console.log(c);
+c/=4;
+console.log(c);
+c%=3;
+console.log(c);
+
+
+let x=10;
+let y="10";
+console.log(x==y);
+console.log(x===y);
+console.log(x!=y);
+console.log(x!==y);
